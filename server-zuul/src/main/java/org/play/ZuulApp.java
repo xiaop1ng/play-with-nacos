883edb80c4fa1bf5c222c237809f2c614eb6f647
xiaop1ng <jianchaoping@gmail.com>
2023-05-31 10:30:30 +0800

    添加网关

---
+++ server-zuul/src/main/java/org/play/ZuulApp.java
@@ -0,0 +1,18 @@
+package org.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableZuulProxy
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApp.class, args);
+    }
+}

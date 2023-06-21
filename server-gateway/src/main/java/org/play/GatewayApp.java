57dfc592a710e98de630a07ba717d94e74f36388
xiaop1ng <jianchaoping@gmail.com>
2023-06-21 10:22:31 +0800

    Update GatewayApp.java

--- server-gateway/src/main/java/org/play/GatewayApp.java
+++ server-gateway/src/main/java/org/play/GatewayApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.play")
 // 开启服务注册发现功能
 @EnableDiscoveryClient
 // 开启 feign 远程方法调用
